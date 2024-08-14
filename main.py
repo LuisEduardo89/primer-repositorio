8b2371569fe5e7d6a7c382752eb63bddbc10bb17
LuisEduardo89 <luiseduardoherreratellez@gmail.com>
2024-08-13 20:40:49 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+memedict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "OFL" - "una respuesta a una broma"
+            "SHEESH" - "ligera desaprobación"
+            "CREEPY" - "aterrador, siniestro"
+            "AGGRO" - "ponerse agresivo/enojado"
+            }
+word = input ("escribe una palabra que no entiendas (¡con mayusculas!")
+
+if word in memedict.keys():
+print(meme_dict[word])
+else:
+    print("no tenemos el significado de la palabra lo sentimos")
